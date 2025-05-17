539d3983b46dac01ed6563318e392ea83bc17685
brenescensus <76098751+brenescensus@users.noreply.github.com>
2025-05-17 23:23:33 +0300

    logo

--- src/app/components/layouts/header.js
+++ src/app/components/layouts/header.js
@@ -1,37 +1,3 @@
-
-// "use client";
-// import React from "react";
-// import Image from "next/image";
-// import Link from "next/link";
-// import { motion } from "framer-motion";
-
-// export default function Header() {
-//   return (
-//     <>
-     
-//       <header className="bg-[#ffffff] text-[#2f2923] sticky top-0 z-50 shadow-xl">
-//         <div className="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center">
-//           <div className="text-2xl font-bold text-indigo-400">
-//             <Image 
-//             src="/Assets/images/digilogo.png"
-//             alt="Digistova Logo"
-//             width={100}
-//             height={70}
-//             />
-//           </div>
-//           <nav className="space-x-6 font-bold text-base">
-//             <Link className="hover:border-b-2 border-white" href="#about">About</Link>
-//             <Link className="hover:border-b-2 border-white" href="#services">Services</Link>
-//             <Link className="hover:border-b-2 border-white" href="#projects">Projects</Link>
-//             <Link className="hover:border-b-2 border-white" href="#contact">Contact</Link>
-//           </nav>
-//         </div>
-//       </header>
-    
-//     </>
-//   )
-// }
-
 "use client";
 import React, { useEffect, useState } from "react";
 import Image from "next/image";
